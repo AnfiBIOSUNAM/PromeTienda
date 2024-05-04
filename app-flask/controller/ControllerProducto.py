b24fd9fb659b0e4e299dc2add2aed26e90f5acf9
IsaacAE <alcantara_estrada_kevin@ciencias.unam.mx>
2024-05-03 20:35:15 -0600

    Re bosquejo para actualizar producto

--- app-flask/controller/ControllerProducto.py
+++ app-flask/controller/ControllerProducto.py
@@ -57,7 +57,7 @@ def update_product():
         idUsuario = request.form.get('idUsuario')
         nombreProducto = request.form.get('nombreProducto')
         descripcion = request.form.get('descripcion')
-        foto = request.form.get('foto')
+        foto = request.form.get('imagen')
         precio = request.form.get('precio')
         contacto = request.form.get('contacto')
         cantidad = request.form.get('cantidad')
@@ -91,3 +91,14 @@ def read_products_by_category(categoria):
     if products == -1:
         return json.dumps({'error': 'No se pudieron obtener los productos de la categoría'})
     return json.dumps([product.to_dict() for product in products])
+
+@producto_blueprint.route('/foto', methods=['GET'])
+def obtener_imagen_producto(idProducto):
+    # Obtén el producto por su ID
+    producto = mp.read_product(idProducto)
+
+    if producto ==-1:
+        return json.dumps({'error': 'No se pudo obtener foto del producto'})
+    else:
+        # Si no se encuentra el producto, devuelve un mensaje de error
+        return json.dumps(producto.to_dict())
