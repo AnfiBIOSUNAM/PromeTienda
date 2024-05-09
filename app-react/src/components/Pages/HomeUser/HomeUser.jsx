71f789e4b735caf66d43fcf6d603afb6c1ac7186
MeliLiz <melifernandez@ciencias.unam.mx>
2024-05-09 13:32:53 -0600

    Productos en home

--- app-react/src/components/Pages/HomeUser/HomeUser.jsx
+++ app-react/src/components/Pages/HomeUser/HomeUser.jsx
@@ -1,10 +1,27 @@
 import { NavLink } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './HomeUser.css'
 import { useCookies } from 'react-cookie';
 
 export default function HomeUser() {
 
     const [cookies, setCookie, removeCookie] = useCookies(['userToken']);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/products')
+        .then(response => {
+            const updatedProducts = response.data.map(product => {
+            return {
+                ...product,
+                fotourl: `http://localhost:5000/imagenes/${product.foto}` // Construir la URL completa aquí
+            };
+            });
+            setProducts(updatedProducts);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+    }, []);
 
     return (
         <>
@@ -24,209 +41,37 @@ export default function HomeUser() {
         <section className="py-5">
             <div className="container px-4 px-lg-5 mt-5">
                 <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                    <div className="col mb-5">
-                        <div className="card h-100">
-                            
-                            <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            
-                            <div className="card-body p-4">
-                                <div className="text-center">
-                                    
-                                    <h5 className="fw-bolder">Fancy Product</h5>
-                                    
-                                    $40.00 - $80.00
-                                </div>
-                            </div>
-                            
-                            <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col mb-5">
-                        <div className="card h-100">
-                            
-                            <div className="badge bg-dark text-white position-absolute" style={{top: 0.5+'rem', right: 0.5+'rem'}}>Sale</div>
-                            
-                            <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            
-                            <div className="card-body p-4">
-                                <div className="text-center">
-                                    
-                                    <h5 className="fw-bolder">Special Item</h5>
-                                    
-                                    <div className="d-flex justify-content-center small text-warning mb-2">
-                                        <div className="bi-star-fill"></div>
-                                        <div className="bi-star-fill"></div>
-                                        <div className="bi-star-fill"></div>
-                                        <div className="bi-star-fill"></div>
-                                        <div className="bi-star-fill"></div>
+                    
+                    {products.slice(0,8).map(product => (
+                        <div className="col mb-5">
+
+                            <div key={product.idProducto} className="card h-100">
+                                
+                                <img className="card-img-top img-fluid img-card" src={product.fotourl} alt={product.nombreProducto} />
+                                
+                                <div className="card-body p-4">
+                                    <div className="text-center">
+                                        
+                                        <h5 className="fw-bolder">{product.nombreProducto}</h5>
+                                        <p>{product.descripcion}</p>
+                                        <p>$ {product.precio}</p>
                                     </div>
-                                    
-                                    <span className="text-muted text-decoration-line-through">$20.00</span>
-                                    $18.00
+                                </div>
+                                
+                                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
                                 </div>
                             </div>
-                            
-                            <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
-                            </div>
                         </div>
-                    </div>
-                    <div className="col mb-5">
-                        <div className="card h-100">
-                            
-                            <div className="badge bg-dark text-white position-absolute" style={{top: 0.5+'rem', right: 0.5+'rem'}}>Sale</div>
-                            
-                            <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            
-                            <div className="card-body p-4">
-                                <div className="text-center">
-                                    
-                                    <h5 className="fw-bolder">Sale Item</h5>
-                                    
-                                    <span className="text-muted text-decoration-line-through">$50.00</span>
-                                    $25.00
-                                </div>
-                            </div>
-                            
-                            <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col mb-5">
-                        <div className="card h-100">
-                            
-                            <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            
-                            <div className="card-body p-4">
-                                <div className="text-center">
-                                    
-                                    <h5 className="fw-bolder">Popular Item</h5>
-                                    
-                                    <div className="d-flex justify-content-center small text-warning mb-2">
-                                        <div className="bi-star-fill"></div>
-                                        <div className="bi-star-fill"></div>
-                                        <div className="bi-star-fill"></div>
-                                        <div className="bi-star-fill"></div>
-                                        <div className="bi-star-fill"></div>
-                                    </div>
-                                    
-                                    $40.00
-                                </div>
-                            </div>
-                            
-                            <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col mb-5">
-                        <div className="card h-100">
-                            
-                            <div className="badge bg-dark text-white position-absolute" style={{top: 0.5+'rem', right: 0.5+'rem'}}>Sale</div>
-                            
-                            <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            
-                            <div className="card-body p-4">
-                                <div className="text-center">
-                                    
-                                    <h5 className="fw-bolder">Sale Item</h5>
-                                    
-                                    <span className="text-muted text-decoration-line-through">$50.00</span>
-                                    $25.00
-                                </div>
-                            </div>
-                            
-                            <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col mb-5">
-                        <div className="card h-100">
-                            
-                            <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            
-                            <div className="card-body p-4">
-                                <div className="text-center">
-                                    
-                                    <h5 className="fw-bolder">Fancy Product</h5>
-                                    
-                                    $120.00 - $280.00
-                                </div>
-                            </div>
-                            
-                            <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col mb-5">
-                        <div className="card h-100">
-                            
-                            <div className="badge bg-dark text-white position-absolute" style={{top: 0.5+'rem', right: 0.5+'rem'}}>Sale</div>
-                            
-                            <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            
-                            <div className="card-body p-4">
-                                <div className="text-center">
-                                    
-                                    <h5 className="fw-bolder">Special Item</h5>
-                                    
-                                    <div className="d-flex justify-content-center small text-warning mb-2">
-                                        <div className="bi-star-fill"></div>
-                                        <div className="bi-star-fill"></div>
-                                        <div className="bi-star-fill"></div>
-                                        <div className="bi-star-fill"></div>
-                                        <div className="bi-star-fill"></div>
-                                    </div>
-                                    
-                                    <span className="text-muted text-decoration-line-through">$20.00</span>
-                                    $18.00
-                                </div>
-                            </div>
-                            
-                            <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col mb-5">
-                        <div className="card h-100">
-                            
-                            <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                            
-                            <div className="card-body p-4">
-                                <div className="text-center">
-                                    
-                                    <h5 className="fw-bolder">Popular Item</h5>
-                                    
-                                    <div className="d-flex justify-content-center small text-warning mb-2">
-                                        <div className="bi-star-fill"></div>
-                                        <div className="bi-star-fill"></div>
-                                        <div className="bi-star-fill"></div>
-                                        <div className="bi-star-fill"></div>
-                                        <div className="bi-star-fill"></div>
-                                    </div>
-                                   
-                                    $40.00
-                                </div>
-                            </div>
-                            
-                            <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
+                    
                 </div>
             </div>
         </section>
 
         <div className='text-center mb-5'>
             {cookies.user['vendedor'] === 0 && (
-                <NavLink to='#' className={'btn btn-primary'} >Ver más productos</NavLink>
+                <NavLink to='/productos/ver' className={'btn btn-primary'} >Ver más productos</NavLink>
             )}
             {cookies.user['vendedor'] === 1 && (
                 <>
